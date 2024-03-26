7d72e930f434a1ef940741bc9a84e00e05317002
Shine <sabirovdima1@gmail.com>
2024-03-27 01:16:19 +0300

    [  Rigs ] added cpu-info model for rig

--- src/entities/rig/model/cpu-info.ts
+++ src/entities/rig/model/cpu-info.ts
@@ -1,6 +1,11 @@
 import { z } from "zod";
 
-
+export const Clocking = z.object({
+  minClock: z.string({invalid_type_error: 'MinClock must be a string'}),
+  maxClock: z.string({invalid_type_error: 'MaxClock must be a string'}),
+  currentClock: z.string({invalid_type_error: 'CurrentClock must be a string'}),
+})
+export type Clocking = z.infer<typeof Clocking>
 
 export const CpuInfo = z.object({
   name: z.string({invalid_type_error: 'Name must be a string'}),
@@ -9,5 +14,6 @@ export const CpuInfo = z.object({
   serialNumber: z.string({invalid_type_error: 'SerialNumber must be a string'}),
   threadsCount: z.number({invalid_type_error: 'ThreadsCount must be a number'}),
   threadsPerSocketCount: z.number({invalid_type_error: 'ThreadsPerSocketCount must be a number'}),
-  clocking: 
+  clocking: Clocking
 })
+export type CpuInfo = z.infer<typeof CpuInfo>
