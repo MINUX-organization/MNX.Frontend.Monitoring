bb79adddd32f7826dec7d0f08a3dade46db020f7
Shine <sabirovdima1@gmail.com>
2024-03-27 10:28:52 +0300

    [ Login ] added navigate when login is true

--- src/pages/login/ui/login.page.tsx
+++ src/pages/login/ui/login.page.tsx
@@ -1,7 +1,19 @@
 import { LoginForm } from '@/features/login/form'
 import styles from './login.module.scss'
+import { useEffect } from 'react'
+import { useSessionRepository } from '@/entities/session'
+import { useLocation, useNavigate } from 'react-router';
 
 export function Login() {
+  const { getSession } = useSessionRepository();
+  const navigate = useNavigate();
+  const session = getSession();
+
+  useEffect(() => {
+    if (!session) return;
+
+    navigate('/');
+  }, [session, navigate])
 
   return (
     <div className={styles['login']}>
