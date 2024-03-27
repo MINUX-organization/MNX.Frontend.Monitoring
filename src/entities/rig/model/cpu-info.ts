19aef3152bc25c91af8dc7420c4525f0ca7e2f1f
Shine <sabirovdima1@gmail.com>
2024-03-27 03:06:05 +0300

    [ Rigs ] added motherboard info model

--- src/entities/rig/model/cpu-info.ts
+++ src/entities/rig/model/cpu-info.ts
@@ -1,19 +1,19 @@
 import { z } from "zod";
 
-export const Clocking = z.object({
+export const RigClocking = z.object({
   minClock: z.string({invalid_type_error: 'MinClock must be a string'}),
   maxClock: z.string({invalid_type_error: 'MaxClock must be a string'}),
   currentClock: z.string({invalid_type_error: 'CurrentClock must be a string'}),
 })
-export type Clocking = z.infer<typeof Clocking>
+export type RigClocking = z.infer<typeof RigClocking>
 
-export const CpuInfo = z.object({
+export const RigCpuInfo = z.object({
   name: z.string({invalid_type_error: 'Name must be a string'}),
   architecture: z.string({invalid_type_error: 'Architecture must be a string'}),
   coresCount: z.number({invalid_type_error: 'CoresCount must be a number'}),
   serialNumber: z.string({invalid_type_error: 'SerialNumber must be a string'}),
   threadsCount: z.number({invalid_type_error: 'ThreadsCount must be a number'}),
   threadsPerSocketCount: z.number({invalid_type_error: 'ThreadsPerSocketCount must be a number'}),
-  clocking: Clocking
+  clocking: RigClocking
 })
-export type CpuInfo = z.infer<typeof CpuInfo>
+export type RigCpuInfo = z.infer<typeof RigCpuInfo>
--- src/entities/rig/model/gpu-info.ts
+++ src/entities/rig/model/gpu-info.ts
@@ -1,16 +1,16 @@
 import { ParallelComputingTechnology } from "@/shared/types/parallel-computing-technology";
 import { z } from "zod";
 
-export const Information = z.object({
+export const RigInformation = z.object({
   pciBusId: z.number({invalid_type_error: 'PciBusId must be a number'}),
   serialNumber: z.string({invalid_type_error: 'SerialNumber must be a string'}),
   memory: z.string({invalid_type_error: 'Vram must be a string'}),
   drivetVersion: z.string({invalid_type_error: 'DrivetVersion must be a string'}),
   parallelComputingTechnology: ParallelComputingTechnology
 })
-export type Information = z.infer<typeof Information>
+export type RigInformation = z.infer<typeof RigInformation>
 
-export const Parameters = z.object({
+export const RigParameters = z.object({
   coreClock: z.string({invalid_type_error: 'CoreClock must be a string'}),
   coreClockOffset: z.string({invalid_type_error: 'CoreClockOffset must be a string'}),
   memoryClock: z.string({invalid_type_error: 'MemoryClock must be a string'}),
@@ -18,9 +18,9 @@ export const Parameters = z.object({
   voltage: z.string({invalid_type_error: 'Voltage must be a string'}),
   voltageOffset: z.string({invalid_type_error: 'VoltageOffset must be a string'}),
 })
-export type Parameters = z.infer<typeof Parameters>
+export type RigParameters = z.infer<typeof RigParameters>
 
-export const Overclocking = z.object({
+export const RigOverclocking = z.object({
   coreClock: z.string({invalid_type_error: 'CoreClock must be a string'}),
   coreClockOffset: z.string({invalid_type_error: 'CoreClockOffset must be a string'}),
   memoryClock: z.string({invalid_type_error: 'MemoryClock must be a string'}),
@@ -33,8 +33,8 @@ export const RigGpusInfo = z.object({
   index: z.number({invalid_type_error: 'Index must be a number'}),
   bus: z.string({invalid_type_error: 'Bus must be a string'}),
   name: z.string({invalid_type_error: 'Name must be a string'}),
-  information: Information,
-  parameters: Parameters,
-  overclocking: Overclocking
+  information: RigInformation,
+  parameters: RigParameters,
+  overclocking: RigOverclocking
 })
 export type RigGpusInfo = z.infer<typeof RigGpusInfo>
