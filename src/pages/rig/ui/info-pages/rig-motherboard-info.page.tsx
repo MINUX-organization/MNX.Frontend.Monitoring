795bb3da9347199ca860126db6b3afb0ecaea254
Shine <sabirovdima1@gmail.com>
2024-03-31 16:42:17 +0300

    [ Motherboard ] added motherboard page for rig

--- src/pages/rig/ui/info-pages/rig-motherboard-info.page.tsx
+++ src/pages/rig/ui/info-pages/rig-motherboard-info.page.tsx
@@ -1,4 +1,4 @@
-import { RigMotherboardInfo } from "@/entities/rig";
+import { RigMotherboardInfo, RigTotalItemMotherboardInfo } from "@/entities/rig";
 import { getRigMotherboardApi } from "@/shared/api/get/getRigMotherboard";
 import { ZodSaveParse } from "@/shared/lib/utils/zod-save-parse";
 import { UiBgContainer } from "@/shared/ui/ui-bg-container";
@@ -6,19 +6,19 @@ import { UiBorderBox } from "@/shared/ui/ui-border-box";
 import { useQuery } from "react-query";
 import { useParams } from "react-router";
 import { match } from "ts-pattern";
-import styles from './rigMotherboardInfo.page.module.scss'
+import styles from './rigMotherboardInfo.module.scss'
 
 export function RigMotherboardInfoPage() {
   const { rigId } = useParams();
   const { data } = useQuery(['rigMotherboardInfo', rigId], () => getRigMotherboardApi(rigId));
-  const validatedData = ZodSaveParse(data, RigMotherboardInfo.array());
+  const validatedData = ZodSaveParse(data, RigMotherboardInfo);
 
   return (
     <UiBorderBox className={styles['rig-motherboard-info']}>
-      <UiBgContainer color="opaque">
+      <UiBgContainer className={styles['flex']} color="opaque">
         {match(validatedData)
           .with(undefined, () => <div className={styles['no-data']}>N/A</div>)
-          .otherwise((validatedData) => )}
+          .otherwise((validatedData) => <RigTotalItemMotherboardInfo rigMotherboard={validatedData}/>)}
       </UiBgContainer>
     </UiBorderBox>
   )
