3eea8badaa290f448588a0c51f5d83b476545d64
Sh1nenium <sabirovdima1@gmail.com>
2024-10-05 23:25:56 +0700

    [ Preset ] delete logic fixed

--- src/features/preset/delete/ui/delete-preset-button.tsx
+++ src/features/preset/delete/ui/delete-preset-button.tsx
@@ -4,6 +4,7 @@ import styles from './delete-preset-button.module.scss';
 import { Preset, usePresetByGpuNameRepository, usePresetRepository } from "@/entities/preset";
 import { useNavigate } from "react-router";
 import { usePresetStateStore } from "@/widgets/preset-modal/model";
+import _ from "lodash";
 
 export function DeletePresetButton({
   className,
@@ -16,9 +17,9 @@ export function DeletePresetButton({
   preset?: Preset;
   isIcon?: boolean;
 }) {
-  const { deletePreset } = usePresetRepository()
+  const { deletePreset, getPresetsList } = usePresetRepository()
   const { deletePresetFromList } = usePresetByGpuNameRepository();
-  const { setPreset, setGpuName } = usePresetStateStore()
+  const { setPreset, setGpuName, selectedPreset } = usePresetStateStore()
   const navigation = useNavigate()
   
   const handler = async (e: React.MouseEvent) => {
@@ -29,11 +30,14 @@ export function DeletePresetButton({
     if (!isSuccess) return;
 
     navigation('')
-    setPreset(undefined)
-    setGpuName(undefined)
 
     if (!preset) return;
     deletePresetFromList(preset)
+
+    if (selectedPreset?.id === presetId) {
+      setPreset(undefined)
+      setGpuName(undefined)
+    }
   }
 
   return (
